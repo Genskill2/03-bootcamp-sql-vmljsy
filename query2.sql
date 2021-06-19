b866dc0d9d73c236ddf273a63905730a36d6e075
vmljsy <vmljsy@proton.com>
2021-06-19 22:16:49 +0530

    fixed

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-select b.title from books b , publisher p where b.publisher = p.id and p.country ='UK';
+select b.title , p.name from books b , publisher p where p.id  = b.publisher and p.country = 'UK';
